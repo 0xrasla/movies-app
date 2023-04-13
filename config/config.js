587b8142038b5c583f8aa516537ea14bab619c3b
rasla <programmer.rasla@gmail.com>
2023-04-13 15:44:36 +0530

    first commit

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const Config = Object.freeze({
-  moviedb: {
-    apiKey: "YOUR_API_KEY",
-    baseUrl: "https://api.themoviedb.org/3",
-    imageBaseUrl: "https://image.tmdb.org/t/p/w500",
-    language: "en-US",
-    region: "US",
-  },
-});
